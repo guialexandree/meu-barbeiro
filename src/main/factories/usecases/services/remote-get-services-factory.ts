4b1acab3b200e862c324ba1c0ae57ea79902bd5d
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-01-13 00:42:38 -0300

    feat: simplify RemoteGetServices instantiation by removing API URL dependency

--- src/main/factories/usecases/services/remote-get-services-factory.ts
+++ src/main/factories/usecases/services/remote-get-services-factory.ts
@@ -1,8 +1,6 @@
 import { GetServices } from '@/domain/usecases'
 import { RemoteGetServices } from '@/data/usecases'
-import { makeAxiosHttpClient } from '@/main/factories/adapters'
 
 export const makeRemoteGetServices = (): GetServices => {
-  const apiUrl = process.env.API_URL || 'http://localhost:3000'
-  return new RemoteGetServices(apiUrl, makeAxiosHttpClient())
+  return new RemoteGetServices()
 }
