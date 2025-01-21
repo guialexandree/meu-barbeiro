b17bc4dd7e36cb5321cd34bf0184a4604f8bb801
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-01-20 22:39:34 -0300

    chore: fix script

--- src/domain/tests/service-mocks.ts
+++ src/domain/tests/service-mocks.ts
@@ -2,7 +2,7 @@ import { ServiceModel } from '@/domain/models'
 import faker from 'faker'
 
 export const mockService: ServiceModel = {
-  id: faker.datatype.number(),
+  id: faker.datatype.uuid(),
   name: faker.name.findName(),
   description: faker.random.words(8),
   price: faker.datatype.number(),
@@ -12,7 +12,7 @@ export const mockService: ServiceModel = {
 
 export const _mockServices: ServiceModel[] = [
   {
-    id: 1,
+    id: '1',
     name: 'Corte',
     description: 'Corte + Escovação',
     price: 30,
@@ -20,7 +20,7 @@ export const _mockServices: ServiceModel[] = [
     status: 'ativo',
   },
   {
-    id: 2,
+    id: '2',
     name: 'Progressiva',
     description: 'Progressiva + Hidratação',
     price: 90,
@@ -28,7 +28,7 @@ export const _mockServices: ServiceModel[] = [
     status: 'inativo',
   },
   {
-    id: 3,
+    id: '3',
     name: 'Reflexo',
     description: 'Reflexo + Hidratação',
     price: 60,
@@ -36,7 +36,7 @@ export const _mockServices: ServiceModel[] = [
     status: 'ativo',
   },
   {
-    id: 4,
+    id: '4',
     name: 'Barba',
     description: '',
     price: 20,
@@ -44,7 +44,7 @@ export const _mockServices: ServiceModel[] = [
     status: 'ativo',
   },
   {
-    id: 5,
+    id: '5',
     name: 'Hidratação',
     description: 'Hidratação',
     price: 20,
@@ -52,7 +52,7 @@ export const _mockServices: ServiceModel[] = [
     status: 'ativo',
   },
   {
-    id: 6,
+    id: '6',
     name: 'Alisamento',
     description: 'Hidratação',
     price: 35,
@@ -60,7 +60,7 @@ export const _mockServices: ServiceModel[] = [
     status: 'ativo',
   },
   {
-    id: 7,
+    id: '7',
     name: 'Botox',
     description: 'Botox + Hidratação',
     price: 40,
