f654bab212a4f189c000ba1fc825de1071109fe3
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-02-26 19:57:43 -0300

    refactor: remove PrivateRoute wrapper from AdminTemplate for improved structure

--- src/presentation/templates/admin-template/index.tsx
+++ src/presentation/templates/admin-template/index.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { AppBar, Drawer, PrivateRoute } from '@/presentation/components'
+import { AppBar, Drawer } from '@/presentation/components'
 import { appNavigation } from '@/main/configs'
 import { Box } from '@mui/material'
 import { ToastContainer } from 'react-toastify'
 
 export const AdminTemplate: React.FC = () => {
   return (
-    <PrivateRoute>
-      <Box
-        sx={{
-          backgroundColor: (theme) => (theme.palette.mode === 'dark' ? 'background.default' : 'inherit'),
-        }}
-      >
-        <AppBar />
-        <Drawer items={appNavigation} />
-        <Outlet />
-        <ToastContainer autoClose={7000} hideProgressBar={false} theme="dark" position="bottom-right" />
-      </Box>
-    </PrivateRoute>
+    <Box
+      sx={{
+        backgroundColor: (theme) => (theme.palette.mode === 'dark' ? 'background.default' : 'inherit'),
+      }}
+    >
+      <AppBar />
+      <Drawer items={appNavigation} />
+      <Outlet />
+      <ToastContainer autoClose={7000} hideProgressBar={false} theme="dark" position="bottom-right" />
+    </Box>
   )
 }
