42faf78d08a2456d6a8d1520fe06179a4764a2ac
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-02-27 00:24:39 -0300

    refactor: remove service worker registration from main entry point

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -8,9 +8,6 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import '@fontsource/roboto/900.css'
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
