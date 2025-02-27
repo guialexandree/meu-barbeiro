f69e2042effbb073541b67a53a8de00454162834
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-02-27 00:22:56 -0300

    refactor: remove Vite PWA plugin and related configuration from Vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve:{
+  resolve: {
     alias: {
-      '@': '/src'
+      '@': '/src',
     },
   },
   define: {
     'process.env': process.env,
   },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Gestão Barbearia',
-        short_name: 'Barbearia',
-        description: 'Aplicativo de gestão para barbearias',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-      workbox: {
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-      }
-    }),
-  ]
+  plugins: [react()],
 })
