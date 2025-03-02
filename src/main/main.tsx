e6c0265cbe49179604c24f1a0e86a20899aaf1f0
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-03-01 21:50:36 -0300

    fix: remove @fontsource/roboto dependency and add Google Fonts link in index.html

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { Router } from '@/main/router'
 import '@/main/configs/clarity'
 import '@/presentation/styles/global.css'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
-import '@fontsource/roboto/900.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
