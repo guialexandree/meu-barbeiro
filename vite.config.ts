f40c7beeae598f22c9f82aea4f080f1d7d5ed951
Guilherme Alexandre <guilherme_alexandree@hotmail.com>
2025-05-17 18:03:45 -0300

    fix: update favicon paths in Vite configuration and add new logo images

--- vite.config.ts
+++ vite.config.ts
@@ -41,17 +41,17 @@ export default defineConfig({
         theme_color: '#2c82d8',
         icons: [
           {
-            src: 'favicon.png',
+            src: 'barber-icons/susuzeira/favicon.png',
             sizes: '64x64',
             type: 'image/png',
           },
           {
-            src: 'favicon_192x192.png',
+            src: 'barber-icons/susuzeira/favicon_192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'favicon_512x512.png',
+            src: 'barber-icons/susuzeira/favicon_512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
